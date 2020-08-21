bd3d1039d6c0e931b37f4e267cb71ae6a07cf2ea
Bargavi  Kanneganti <bargavikanneganti@Bargavis-MBP.attlocal.net>
2020-08-21 16:08:59 -0500

    updated tip calc

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,6 +1,7 @@
 total_bill = float(input('Total bill amount?'))
 level_of_service = input('Level of service?')
 tip_amount = total_bill * (.10 or .20 or .15)
+total_amount = total_bill + tip_amount
 
 if level_of_service == "good":
     tip_amount == (.10 * total_bill)
@@ -11,4 +12,4 @@ elif level_of_service == "fair":
 else:
     tip_amount == (.10 * total_bill)
     print("Tip amount:" + str((.10 * total_bill)))
-print("Total amount:" + (tip_amount * total_bill) + total_bill)
+print(total_amount)
